1c41d6aa0c414bcf3c979f26b771e933f6517757
tranducnam0612 <tranducnam0612@gmail.com>
2021-06-18 08:08:11 +0700

    merge code 18/06

--- lib/screen/authen/login/login_controller.dart
+++ lib/screen/authen/login/login_controller.dart
@@ -126,21 +126,20 @@ class LoginController extends GetxController {
     }
     update();
 
-  String timeAgo(int timestamp) {
-    var date = new DateTime.fromMicrosecondsSinceEpoch(timestamp * 1000 * 1000);
-    var now = new DateTime.now();
-    var format = new DateFormat('yyyy-MM-dd HH:mm:ss.SSS');
-    var time = DateTime.parse(format.format(date));
-    var diff = now.difference(time)
-    ;
-    if (diff.inDays > 365) return "${(diff.inDays / 365).floor()} ${(diff.inDays / 365).floor() == 1 ? "năm" : "năm"} trước";
-    if (diff.inDays > 30) return "${(diff.inDays / 30).floor()} ${(diff.inDays / 30).floor() == 1 ? "tháng" : "tháng"} trước";
-    if (diff.inDays > 7) return "${(diff.inDays / 7).floor()} ${(diff.inDays / 7).floor() == 1 ? "tuần" : "tuần"} trước";
-    if (diff.inDays > 0) return "${diff.inDays} ${diff.inDays == 1 ? "ngày" : "ngày"} trước";
-    if (diff.inHours > 0) return "${diff.inHours} ${diff.inHours == 1 ? "giờ" : "giờ"} trước";
-    if (diff.inMinutes > 0) return "${diff.inMinutes} ${diff.inMinutes == 1 ? "phút" : "phút"} trước";
-    return "vừa xong";
-  }
+    String timeAgo(int timestamp) {
+      var date = new DateTime.fromMicrosecondsSinceEpoch(timestamp * 1000 * 1000);
+      var now = new DateTime.now();
+      var format = new DateFormat('yyyy-MM-dd HH:mm:ss.SSS');
+      var time = DateTime.parse(format.format(date));
+      var diff = now.difference(time);
+      if (diff.inDays > 365) return "${(diff.inDays / 365).floor()} ${(diff.inDays / 365).floor() == 1 ? "năm" : "năm"} trước";
+      if (diff.inDays > 30) return "${(diff.inDays / 30).floor()} ${(diff.inDays / 30).floor() == 1 ? "tháng" : "tháng"} trước";
+      if (diff.inDays > 7) return "${(diff.inDays / 7).floor()} ${(diff.inDays / 7).floor() == 1 ? "tuần" : "tuần"} trước";
+      if (diff.inDays > 0) return "${diff.inDays} ${diff.inDays == 1 ? "ngày" : "ngày"} trước";
+      if (diff.inHours > 0) return "${diff.inHours} ${diff.inHours == 1 ? "giờ" : "giờ"} trước";
+      if (diff.inMinutes > 0) return "${diff.inMinutes} ${diff.inMinutes == 1 ? "phút" : "phút"} trước";
+      return "vừa xong";
+    }
 
 //
 // Future<void> getListData() async {
@@ -169,10 +168,11 @@ class LoginController extends GetxController {
 //   }
 //   update();
 // }
-  @override
-  void onInit() {
-    print(userType);
-    listCitySubject();
-    super.onInit();
+    @override
+    void onInit() {
+      print(userType);
+      listCitySubject();
+      super.onInit();
+    }
   }
 }
